5aecad878c7a12d74e7ddaea4057fe741df6b661
Morgan Gangwere <morgan.gangwere@gmail.com>
2023-05-25 21:30:06 -0700

    more work on creating

--- parc/cmd/create.go
+++ parc/cmd/create.go
@@ -5,7 +5,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/spf13/cobra"
 )
 
@@ -21,6 +23,7 @@ parc create myarchive.pzarc a/* b/*`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 2 {
 			fmt.Fprintln(cmd.ErrOrStderr(), "Expected 2 arguments, at least")
+			return
 		}
 
 		// we're going to just list the files out so far
@@ -35,7 +38,24 @@ parc create myarchive.pzarc a/* b/*`,
 		for _, pathn := range archivePaths {
 			// walk every file in those paths.
 
-			fmt.Println(pathn)
+			pathstat, err := os.Lstat(pathn)
+			if err != nil {
+				fmt.Fprintf(cmd.ErrOrStderr(), "Couldn't stat %v, %v\n", pathn, err)
+				continue
+			}
+
+			if pathstat.IsDir() {
+				fmt.Println("Found a directory: " + pathstat.Name())
+			} else {
+
+				if pathstat.Mode()&os.ModeSymlink != 0 {
+					target, _ := os.Readlink(pathn)
+					fmt.Printf("%v -> link to %v\n", pathn, target)
+				} else {
+					spew.Dump(pathstat)
+				}
+
+			}
 
 		}
 
